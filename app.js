44b0ff1b87ff5d381549c45ab8ead9ef10cad940
Bushra-11 <bu2004hu@gmail.com>
2026-06-29 15:45:21 +0300

    Add lives logic in js, and refactor car movement

--- app.js
+++ app.js
@@ -11,12 +11,19 @@ frogElement.style.top = frogTop + "px"
 frogElement.style.left = frogLeft + "px"
 
 //Initial car position
-let carLeft = 0
-carOneElement.style.left = carLeft + 'px'
-carTwoElement.style.left = carLeft + 'px'
-carThreeElement.style.left = carLeft + 'px'
+let carOneLeft = 0
+let carTwoLeft = 0
+let carThreeLeft = 0
+
+//Intial number of lives
+let lives = 3
+
+carOneElement.style.left = carOneLeft + 'px'
+carTwoElement.style.left = carTwoLeft + 'px'
+carThreeElement.style.left = carThreeLeft + 'px'
 
 function move(event){
+
     if(event.key === 'ArrowUp'){
         if(frogTop > 0){
         frogTop -= 60
@@ -44,31 +51,54 @@ function move(event){
         frogElement.style.left = frogLeft + 'px'
         }
     }
-}
 
+    checkCollision()
+
+}
 function carOneMovement(){
-    carLeft += 7
-    if(carLeft > 510){
-        carLeft= 0
+
+    carOneLeft += 7    
+    if(carOneLeft > 510){
+        carOneLeft= 0
     }
-        carOneElement.style.left = carLeft + 'px'
+        carOneElement.style.left = carOneLeft + 'px'
+        checkCollision()
+
 }
 
 function carTwoMovement(){
-    carLeft += 30
-    if(carLeft > 510){
-        carLeft= 0
+    carTwoLeft += 30
+    if(carTwoLeft > 510){
+        carTwoLeft= 0
     }
-        carTwoElement.style.left = carLeft + 'px'
+        carTwoElement.style.left = carTwoLeft + 'px'
+        checkCollision()
+
 }
 
 function carThreeMovement(){
-    carLeft += 20
-    if(carLeft > 510){
-        carLeft= 0
+    carThreeLeft += 20
+    if(carThreeLeft > 510){
+        carThreeLeft= 0
     }
-        carThreeElement.style.left = carLeft + 'px'
+        carThreeElement.style.left = carThreeLeft + 'px'
+        checkCollision()
+
 }
+
+function checkCollision(){
+    if(frogLeft === carOneLeft && frogTop === Math.abs(carOneLeft)){
+        decrementLives()
+    }
+}
+
+function decrementLives(){
+    lives -= 1
+    //reset the frog position again
+    frogTop = 580
+    frogLeft = 260
+}
+
 
 // set interval for the cars to move
 setInterval(carOneMovement,100)
