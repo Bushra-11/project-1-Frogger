6b409bd6146089c764016190dcb1322e98832360
Bushra-11 <bu2004hu@gmail.com>
2026-06-29 13:06:01 +0300

    add frog movement functionality

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('Hello')
+const frogElement = document.querySelector('#frog')
+
+let frogTop = 580
+let frogLeft = 260
+frogElement.style.top = frogTop + "px"
+frogElement.style.left = frogLeft + "px"
+
+function move(event){
+    if(event.key === 'ArrowUp'){
+        if(frogTop > 0){
+        frogTop -= 60
+        frogElement.style.top = frogTop + 'px'
+        }
+    }
+
+    if(event.key === 'ArrowDown'){
+        if(frogTop < 580){
+        frogTop += 60
+        frogElement.style.top = frogTop +'px'
+        }
+    }
+
+    if(event.key === 'ArrowLeft'){
+        if(frogLeft > 55){
+        frogLeft -= 60
+        frogElement.style.left = frogLeft + 'px'
+        }
+    }
+
+    if(event.key === 'ArrowRight'){
+        if(frogLeft < 500){
+        frogLeft += 60
+        frogElement.style.left = frogLeft + 'px'
+        }
+    }
+}
+
+
+// Event Listener
+document.addEventListener('keydown',move)
