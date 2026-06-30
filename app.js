33e9ce60076c44eb2250492e892930ed052f5179
Bushra-11 <bu2004hu@gmail.com>
2026-06-30 15:28:27 +0300

    Refactor frog movement logic and implement log riding functionality

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ let logTwoPosition = 250
 //Intial number of lives
 let lives = 3
 
+// Intial state for the frog before riding the log
+let isOnLogOne = false
+let isOnLogTwo = false
+
+
+
 carOneElement.style.left = carOneLeft + 'px'
 carTwoElement.style.left = carTwoLeft + 'px'
 carThreeElement.style.left = carThreeLeft + 'px'
@@ -34,99 +40,137 @@ carThreeElement.style.left = carThreeLeft + 'px'
 logOneElement.style.left = logOnePosition + 'px'
 logTwoElement.style.left = logTwoPosition + 'px'
 
-function move(event){
+function move(event) {
 
-    if(event.key === 'ArrowUp'){
-        if(frogTop > 0){
-        frogTop -= 60
-        frogElement.style.top = frogTop + 'px'
+    if (event.key === 'ArrowUp') {
+        if (frogTop > 0) {
+            frogTop -= 60
+            frogElement.style.top = frogTop + 'px'
         }
     }
 
-    if(event.key === 'ArrowDown'){
-        if(frogTop < 580){
-        frogTop += 60
-        frogElement.style.top = frogTop +'px'
+    if (event.key === 'ArrowDown') {
+        if (frogTop < 580) {
+            frogTop += 60
+            frogElement.style.top = frogTop + 'px'
         }
     }
 
-    if(event.key === 'ArrowLeft'){
-        if(frogLeft > 55){
-        frogLeft -= 60
-        frogElement.style.left = frogLeft + 'px'
+    if (event.key === 'ArrowLeft') {
+        if (frogLeft > 55) {
+            frogLeft -= 60
+            frogElement.style.left = frogLeft + 'px'
         }
     }
 
-    if(event.key === 'ArrowRight'){
-        if(frogLeft < 500){
-        frogLeft += 60
-        frogElement.style.left = frogLeft + 'px'
+    if (event.key === 'ArrowRight') {
+        if (frogLeft < 500) {
+            frogLeft += 60
+            frogElement.style.left = frogLeft + 'px'
         }
     }
 
-    lilyPadElements.forEach((lily)=>{
+    lilyPadElements.forEach((lily) => {
         // check if frog position overlaps this lilypad
         let lilypadRect = lily.getBoundingClientRect()
         let frogRect = frogElement.getBoundingClientRect()
 
-        if(frogRect.left < lilypadRect.right &&
+        if (frogRect.left < lilypadRect.right &&
             frogRect.right > lilypadRect.left &&
             frogRect.top < lilypadRect.bottom &&
-            frogRect.bottom > lilypadRect.top){
+            frogRect.bottom > lilypadRect.top) {
             landOnLily(lily)
         }
     })
-}
-function carOneMovement(){
 
-    carOneLeft += 7    
-    if(carOneLeft > 510){
-        carOneLeft= 0
+    rideLogOne()
+    rideLogTwo()
+}
+
+function carOneMovement() {
+
+    carOneLeft += 7
+    if (carOneLeft > 510) {
+        carOneLeft = 0
     }
-        carOneElement.style.left = carOneLeft + 'px'
+    carOneElement.style.left = carOneLeft + 'px'
 
 }
 
-function carTwoMovement(){
+function carTwoMovement() {
     carTwoLeft += 30
-    if(carTwoLeft > 510){
-        carTwoLeft= 0
+    if (carTwoLeft > 510) {
+        carTwoLeft = 0
     }
-        carTwoElement.style.left = carTwoLeft + 'px'
+    carTwoElement.style.left = carTwoLeft + 'px'
 
 }
 
-function carThreeMovement(){
+function carThreeMovement() {
     carThreeLeft += 20
-    if(carThreeLeft > 510){
-        carThreeLeft= 0
+    if (carThreeLeft > 510) {
+        carThreeLeft = 0
     }
-        carThreeElement.style.left = carThreeLeft + 'px'
+    carThreeElement.style.left = carThreeLeft + 'px'
 }
 
-function logOneMovement(){
+function logOneMovement() {
     logOnePosition += 20
-    if(logOnePosition > 490){
-        logOnePosition= 0
+    if (logOnePosition > 490) {
+        logOnePosition = 0
     }
     logOneElement.style.left = logOnePosition + 'px'
+
+    //to move the frog along with the code
+    if (isOnLogOne) {
+        frogLeft += 20
+        frogElement.style.left = frogLeft + 'px'
+    }
+    rideLogOne()
+
 }
 
-function logTwoMovement(){
+function logTwoMovement() {
     logTwoPosition += 10
-    if(logTwoPosition > 490){
+    if (logTwoPosition > 490) {
         logTwoPosition = 0
     }
+    logTwoElement.style.left = logTwoPosition + 'px'
 
-logTwoElement.style.left = logTwoPosition + 'px'
+    if (isOnLogTwo) {
+        frogLeft += 10
+        frogElement.style.left = frogLeft + 'px'
+    }
+    rideLogTwo()
 }
 
-function checkCollision(){
-    // if postion of the frog = position of the car you lose 
-        decrementLives()
-    }
+function rideLogOne() {
+    let logOneLocation = logOneElement.getBoundingClientRect()
+    let frogRect = frogElement.getBoundingClientRect()
 
-function decrementLives(){
+    isOnLogOne = (frogRect.left < logOneLocation.right &&
+        frogRect.right > logOneLocation.left &&
+        frogRect.top < logOneLocation.bottom &&
+        frogRect.bottom > logOneLocation.top)
+
+}
+
+function rideLogTwo() {
+    let logTwoLocation = logTwoElement.getBoundingClientRect()
+    let frogRect = frogElement.getBoundingClientRect()
+
+    isOnLogTwo = (frogRect.left < logTwoLocation.right &&
+        frogRect.right > logTwoLocation.left &&
+        frogRect.top < logTwoLocation.bottom &&
+        frogRect.bottom > logTwoLocation.top)
+}
+
+function checkCollision() {
+    // if postion of the frog = position of the car you lose 
+    decrementLives()
+}
+
+function decrementLives() {
     lives -= 1
     //reset the frog position again
     frogTop = 580
@@ -138,7 +182,7 @@ function decrementLives(){
     logTwoPosition = 0
 }
 
-function landOnLily(lily){
+function landOnLily(lily) {
     lily.classList.add('visited')
     let allVisited = [...lilyPadElements].every(pad => pad.classList.contains('visited'))
     if (allVisited) {
@@ -146,19 +190,19 @@ function landOnLily(lily){
     }
 }
 
-function win(){
+function win() {
 
 }
 
 // set interval for the cars to move
-//setInterval(carOneMovement,100)
-//setInterval(carTwoMovement,700)
-//setInterval(carThreeMovement,1200)
+setInterval(carOneMovement, 100)
+setInterval(carTwoMovement, 700)
+setInterval(carThreeMovement, 1200)
 
 //set interval for the logs to move
-//setInterval(logOneMovement,500)
-//setInterval(logTwoMovement,300)
+setInterval(logOneMovement, 500)
+setInterval(logTwoMovement, 300)
 
 
 // Event Listener
-document.addEventListener('keydown',move)
+document.addEventListener('keydown', move)
